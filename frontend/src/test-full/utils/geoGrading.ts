555755c37fcbfae9e4555008dd8c42d7971927b1
alkisax <alkisax@gmail.com>
2026-02-16 23:34:28 +0200

    minor

--- frontend/src/test-full/utils/geoGrading.ts
+++ frontend/src/test-full/utils/geoGrading.ts
@@ -1,4 +1,4 @@
-import type { GradedPoint, LabelCheckResult, MapPoint } from "../types/geographyFull.types"
+import type { GradedPoint, LabelCheckResult, MapPoint } from "../types/Full.types"
 import { simplifyLang } from "./simplifyLang"
 
 // GRADING (ΜΟΝΟ ΣΗΜΕΙΟ)
