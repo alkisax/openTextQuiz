977bfca56fc902997dba0e543e4dfa2ca0f8e70f
alkisax <alkisax@gmail.com>
2026-02-20 11:12:53 +0200

    wip: fix on opera render matching questions

--- frontend/src/test-full/components/MatchingQuestion.tsx
+++ frontend/src/test-full/components/MatchingQuestion.tsx
@@ -1,10 +1,10 @@
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
 import {
   Table,
   TableBody,
